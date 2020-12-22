d45b31e84b2ebc80f39f422938d88d825b750cab
Oswaldo Lisboa <oswaldolisboa@MacBook-Air-de-Oswaldo.local>
2020-12-22 12:36:53 +0100

    Removed data.py from .gitIgnore and removed tracking from jpg files.

---
+++ data.py
@@ -0,0 +1,190 @@
+TEAMS = ["Athletico Paranaense","Atlético Goianiense","Atlético Mineiro","Bahia","Botafogo","Ceará","Corinthians","Coritiba","Flamengo","Fluminense","Fortaleza","Goiás","Grêmio","Internacional","Palmeiras","Red Bull Bragantino","Santos","São Paulo","Sport","Vasco"]
+TWITTERS = ["@AthleticoPR","@ACGOficial","@Atletico","@ECBahia","@Botafogo","@CearaSC","@Corinthians","@Coritiba","@Flamengo","@FluminenseFC","@FortalezaEC","@goiasoficial","@Gremio","@SCInternacional","@Palmeiras","@RedBullBraga","@SantosFC","@SaoPauloFC","@sportrecife","@VascodaGama"]
+CRESTS = ["crests/athletico.png","crests/atletico-goianiense.png","crests/atletico-mineiro.png","crests/bahia.png","crests/botafogo.png","crests/ceara.png","crests/corinthians.png","crests/coritiba.png","crests/flamengo.png","crests/fluminense.png","crests/fortaleza.png","crests/goias.png","crests/gremio.png","crests/internacional.png","crests/palmeiras.png","crests/rb-bragantino.png","crests/santos.png","crests/sao-paulo.png","crests/sport.png","crests/vasco.png"]
+
+HOME_SCORES = [0] * 245 + [1] * 379 + [2] * 219 + [3] * 95 + [4] * 47 + [5] * 8 + [6] * 4 + [7] * 2 + [8] * 1
+AWAY_SCORES = [0] * 379 + [1] * 395 + [2] * 168 + [3] * 40 + [4] * 11 + [5] * 5 + [6] * 1 + [7] * 1
+
+MATCHDAYS = [1,1,1,1,1,1,1,1,1,1,2,2,2,2,2,2,2,2,2,2,3,3,3,3,3,3,3,3,3,3,4,4,4,4,4,4,4,4,4,4,5,5,5,5,5,5,5,5,5,5,6,6,6,6,6,6,6,6,6,6,7,7,7,7,7,7,7,7,7,7,8,8,8,8,8,8,8,8,8,8,9,9,9,9,9,9,9,9,9,9,10,10,10,10,10,10,10,10,10,10,11,11,11,11,11,11,11,11,11,11,12,12,12,12,12,12,12,12,12,12,13,13,13,13,13,13,13,13,13,13,14,14,14,14,14,14,14,14,14,14,15,15,15,15,15,15,15,15,15,15,16,16,16,16,16,16,16,16,16,16,17,17,17,17,17,17,17,17,17,17,18,18,18,18,18,18,18,18,18,18,19,19,19,19,19,19,19,19,19,19,20,20,20,20,20,20,20,20,20,20,21,21,21,21,21,21,21,21,21,21,22,22,22,22,22,22,22,22,22,22,23,23,23,23,23,23,23,23,23,23,24,24,24,24,24,24,24,24,24,24,25,25,25,25,25,25,25,25,25,25,26,26,26,26,26,26,26,26,26,26,27,27,27,27,27,27,27,27,27,27,28,28,28,28,28,28,28,28,28,28,29,29,29,29,29,29,29,29,29,29,30,30,30,30,30,30,30,30,30,30,31,31,31,31,31,31,31,31,31,31,32,32,32,32,32,32,32,32,32,32,33,33,33,33,33,33,33,33,33,33,34,34,34,34,34,34,34,34,34,34,35,35,35,35,35,35,35,35,35,35,36,36,36,36,36,36,36,36,36,36,37,37,37,37,37,37,37,37,37,37,38,38,38,38,38,38,38,38,38,38]
+
+HOME = ["Flamengo","Botafogo","Palmeiras","Santos","Corinthians","Grêmio","Sport","Coritiba","Fortaleza","Goiás","Fluminense","Vasco","São Paulo","Red Bull Bragantino","Atlético Mineiro","Internacional","Bahia","Athletico Paranaense","Ceará","Atlético Goianiense","Fluminense","Vasco","Palmeiras","Santos","Atlético Mineiro","Grêmio","Bahia","Coritiba","Fortaleza","Atlético Goianiense","Flamengo","Botafogo","São Paulo","Red Bull Bragantino","Corinthians","Internacional","Sport","Athletico Paranaense","Ceará","Goiás","Flamengo","Vasco","Palmeiras","Red Bull Bragantino","Corinthians","Internacional","Sport","Athletico Paranaense","Ceará","Goiás","Fluminense","Botafogo","São Paulo","Santos","Atlético Mineiro","Grêmio","Bahia","Coritiba","Fortaleza","Atlético Goianiense","Fluminense","Botafogo","Palmeiras","Santos","Atlético Mineiro","Grêmio","Bahia","Athletico Paranaense","Ceará","Goiás","Flamengo","Vasco","São Paulo","Red Bull Bragantino","Corinthians","Internacional","Sport","Coritiba","Ceará","Atlético Goianiense","Fluminense","Vasco","São Paulo","Santos","Corinthians","Internacional","Bahia","Athletico Paranaense","Fortaleza","Goiás","Fluminense","Botafogo","Palmeiras","Santos","Atlético Mineiro","Grêmio","Bahia","Athletico Paranaense","Ceará","Goiás","Flamengo","Botafogo","São Paulo","Red Bull Bragantino","Corinthians","Grêmio","Sport","Coritiba","Fortaleza","Atlético Goianiense","Fluminense","Vasco","Palmeiras","Santos","Atlético Mineiro","Internacional","Sport","Athletico Paranaense","Ceará","Atlético Goianiense","Flamengo","Botafogo","Palmeiras","Red Bull Bragantino","Atlético Mineiro","Grêmio","Bahia","Coritiba","Fortaleza","Goiás","Flamengo","Botafogo","São Paulo","Red Bull Bragantino","Corinthians","Grêmio","Bahia","Athletico Paranaense","Fortaleza","Goiás","Fluminense","Vasco","Palmeiras","Santos","Atlético Mineiro","Internacional","Sport","Coritiba","Ceará","Atlético Goianiense","Flamengo","Vasco","Palmeiras","Santos","Atlético Mineiro","Grêmio","Sport","Athletico Paranaense","Ceará","Goiás","Fluminense","Botafogo","São Paulo","Red Bull Bragantino","Corinthians","Internacional","Bahia","Coritiba","Fortaleza","Atlético Goianiense","Fluminense","Vasco","São Paulo","Red Bull Bragantino","Atlético Mineiro","Internacional","Bahia","Athletico Paranaense","Ceará","Atlético Goianiense","Flamengo","Botafogo","Palmeiras","Santos","Corinthians","Grêmio","Sport","Coritiba","Fortaleza","Goiás","Fluminense","Vasco","São Paulo","Red Bull Bragantino","Atlético Mineiro","Internacional","Bahia","Athletico Paranaense","Ceará","Atlético Goianiense","Flamengo","Botafogo","Palmeiras","Santos","Corinthians","Grêmio","Sport","Coritiba","Fortaleza","Goiás","Flamengo","Botafogo","São Paulo","Red Bull Bragantino","Corinthians","Internacional","Sport","Athletico Paranaense","Ceará","Goiás","Fluminense","Vasco","Palmeiras","Santos","Atlético Mineiro","Grêmio","Bahia","Coritiba","Fortaleza","Atlético Goianiense","Fluminense","Botafogo","São Paulo","Santos","Atlético Mineiro","Grêmio","Bahia","Coritiba","Fortaleza","Atlético Goianiense","Flamengo","Vasco","Palmeiras","Red Bull Bragantino","Corinthians","Internacional","Sport","Athletico Paranaense","Ceará","Goiás","Flamengo","Vasco","São Paulo","Red Bull Bragantino","Corinthians","Internacional","Sport","Coritiba","Fortaleza","Atlético Goianiense","Fluminense","Botafogo","Palmeiras","Santos","Atlético Mineiro","Grêmio","Bahia","Athletico Paranaense","Fortaleza","Goiás","Flamengo","Botafogo","Palmeiras","Red Bull Bragantino","Atlético Mineiro","Grêmio","Sport","Coritiba","Ceará","Atlético Goianiense","Flamengo","Vasco","São Paulo","Red Bull Bragantino","Corinthians","Internacional","Sport","Coritiba","Fortaleza","Atlético Goianiense","Fluminense","Vasco","Palmeiras","Santos","Atlético Mineiro","Internacional","Bahia","Athletico Paranaense","Ceará","Goiás","Flamengo","Botafogo","São Paulo","Red Bull Bragantino","Corinthians","Grêmio","Bahia","Coritiba","Fortaleza","Goiás","Fluminense","Vasco","São Paulo","Santos","Corinthians","Internacional","Sport","Athletico Paranaense","Ceará","Atlético Goianiense","Fluminense","Vasco","Palmeiras","Santos","Atlético Mineiro","Internacional","Sport","Coritiba","Ceará","Atlético Goianiense","Flamengo","Botafogo","São Paulo","Red Bull Bragantino","Corinthians","Grêmio","Bahia","Athletico Paranaense","Fortaleza","Goiás","Fluminense","Botafogo","São Paulo","Red Bull Bragantino","Corinthians","Internacional","Bahia","Coritiba","Fortaleza","Atlético Goianiense","Flamengo","Vasco","Palmeiras","Santos","Atlético Mineiro","Grêmio","Sport","Athletico Paranaense","Ceará","Goiás","Flamengo","Botafogo","Palmeiras","Santos","Corinthians","Grêmio","Sport","Coritiba","Fortaleza","Goiás","Fluminense","Vasco","São Paulo","Red Bull Bragantino","Atlético Mineiro","Internacional","Bahia","Athletico Paranaense","Ceará","Atlético Goianiense"]
+
+AWAY = ["Atlético Mineiro","Bahia","Vasco","Red Bull Bragantino","Atlético Goianiense","Fluminense","Ceará","Internacional","Athletico Paranaense","São Paulo","Palmeiras","Sport","Fortaleza","Botafogo","Corinthians","Santos","Coritiba","Goiás","Grêmio","Flamengo","Internacional","São Paulo","Goiás","Athletico Paranaense","Ceará","Corinthians","Red Bull Bragantino","Flamengo","Botafogo","Sport","Grêmio","Atlético Mineiro","Bahia","Fluminense","Coritiba","Atlético Goianiense","Santos","Palmeiras","Vasco","Fortaleza","Botafogo","Grêmio","Santos","Coritiba","Fortaleza","Atlético Mineiro","São Paulo","Fluminense","Bahia","Atlético Goianiense","Vasco","Internacional","Corinthians","Flamengo","Athletico Paranaense","Goiás","Palmeiras","Sport","Red Bull Bragantino","Ceará","Atlético Goianiense","Coritiba","Internacional","Vasco","São Paulo","Sport","Flamengo","Red Bull Bragantino","Fortaleza","Corinthians","Fortaleza","Athletico Paranaense","Fluminense","Palmeiras","Botafogo","Bahia","Goiás","Atlético Mineiro","Santos","Grêmio","Flamengo","Atlético Goianiense","Red Bull Bragantino","Atlético Mineiro","Palmeiras","Ceará","Grêmio","Botafogo","Sport","Coritiba","Corinthians","Vasco","Sport","São Paulo","Red Bull Bragantino","Fortaleza","Atlético Goianiense","Coritiba","Flamengo","Internacional","Goiás","Santos","Athletico Paranaense","Ceará","Bahia","Palmeiras","Fluminense","Vasco","Internacional","Atlético Mineiro","Coritiba","Red Bull Bragantino","Flamengo","Fortaleza","Grêmio","São Paulo","Corinthians","Bahia","Goiás","Botafogo","Athletico Paranaense","Fluminense","Ceará","Corinthians","Vasco","Internacional","Sport","São Paulo","Atlético Goianiense","Santos","Sport","Palmeiras","Atlético Goianiense","Internacional","Santos","Coritiba","Vasco","Ceará","Atlético Mineiro","Fluminense","Bahia","Flamengo","São Paulo","Grêmio","Goiás","Athletico Paranaense","Botafogo","Fortaleza","Corinthians","Red Bull Bragantino","Red Bull Bragantino","Fortaleza","Coritiba","Atlético Goianiense","Fluminense","Botafogo","Internacional","Corinthians","São Paulo","Bahia","Ceará","Goiás","Grêmio","Sport","Flamengo","Vasco","Atlético Mineiro","Santos","Palmeiras","Athletico Paranaense","Santos","Corinthians","Botafogo","Goiás","Sport","Flamengo","Fortaleza","Grêmio","Coritiba","Palmeiras","São Paulo","Ceará","Atlético Mineiro","Bahia","Internacional","Red Bull Bragantino","Athletico Paranaense","Atlético Goianiense","Fluminense","Vasco","Grêmio","Palmeiras","Goiás","Santos","Flamengo","Coritiba","Botafogo","Fortaleza","Sport","Corinthians","Atlético Goianiense","Red Bull Bragantino","Fluminense","Internacional","Atlético Mineiro","Ceará","Vasco","Bahia","São Paulo","Athletico Paranaense","Coritiba","Fortaleza","Vasco","Bahia","Grêmio","Fluminense","Atlético Goianiense","Santos","Atlético Mineiro","Palmeiras","Red Bull Bragantino","Ceará","Athletico Paranaense","Sport","Botafogo","Flamengo","São Paulo","Corinthians","Goiás","Internacional","Athletico Paranaense","Flamengo","Sport","Palmeiras","Internacional","Vasco","Ceará","Red Bull Bragantino","Corinthians","Goiás","Santos","Fluminense","Bahia","Fortaleza","São Paulo","Botafogo","Coritiba","Atlético Mineiro","Atlético Goianiense","Grêmio","Bahia","Santos","Atlético Mineiro","Athletico Paranaense","Goiás","Palmeiras","Grêmio","Botafogo","Ceará","Fluminense","São Paulo","Corinthians","Red Bull Bragantino","Ceará","Coritiba","Atlético Goianiense","Internacional","Vasco","Flamengo","Sport","Fluminense","Athletico Paranaense","Corinthians","São Paulo","Santos","Bahia","Fortaleza","Goiás","Internacional","Vasco","Ceará","Botafogo","Santos","Atlético Mineiro","Fluminense","Goiás","Palmeiras","Athletico Paranaense","Grêmio","Bahia","Sport","Coritiba","Grêmio","Botafogo","Atlético Goianiense","Fortaleza","Corinthians","São Paulo","Red Bull Bragantino","Flamengo","Palmeiras","Atlético Goianiense","Internacional","Vasco","Sport","Atlético Mineiro","Athletico Paranaense","Fluminense","Santos","Ceará","Botafogo","Atlético Mineiro","Coritiba","Goiás","Red Bull Bragantino","Grêmio","Bahia","Flamengo","Palmeiras","Fortaleza","Goiás","Bahia","Botafogo","Corinthians","Fortaleza","Red Bull Bragantino","Flamengo","Grêmio","Athletico Paranaense","São Paulo","Vasco","Sport","Palmeiras","Atlético Goianiense","Ceará","Santos","Fluminense","Internacional","Coritiba","Atlético Mineiro","Atlético Mineiro","Grêmio","Ceará","Flamengo","Athletico Paranaense","Sport","Goiás","Palmeiras","Vasco","Santos","Corinthians","Internacional","Fortaleza","Coritiba","Bahia","São Paulo","Red Bull Bragantino","Atlético Goianiense","Fluminense","Botafogo","Internacional","São Paulo","Atlético Goianiense","Fluminense","Vasco","Athletico Paranaense","Atlético Mineiro","Ceará","Bahia","Red Bull Bragantino","Fortaleza","Goiás","Flamengo","Grêmio","Palmeiras","Corinthians","Santos","Sport","Botafogo","Coritiba"]
+
+CSS =  """
+<style type=\"text/css\">
+table {
+color: #333;
+font-family: Helvetica, Arial, sans-serif;
+width: 400px;
+border-collapse:
+collapse;
+border-spacing: 0;
+}
+td, th {
+border: 1px solid transparent; /* No more visible border */
+height: 30px;
+width: 100px;
+}
+th {
+background: #DFDFDF; /* Darken header a bit */
+font-weight: bold;
+text-align: center;
+}
+td {
+background: #FAFAFA;
+text-align: center;
+}
+table tr:nth-child(odd) td{
+background-color: white;
+}
+</style>
+"""
+
+NEXT_MATCH_HTML = """<style>
+
+  body {{
+    color: #FFFFFF;
+    background: 	#90EE90;
+    font-family: Helvetica, Arial, sans-serif;
+    text-align: center;
+  }}
+
+  table {{
+    border-spacing: 20;
+    text-align: center;
+    width:70%;
+    margin-left:15%;
+    margin-right:15%;
+  }}
+
+  td, th {{
+    border: 1px solid transparent;
+    height: 30px;
+  }}
+
+  img {{
+      width: 300px;
+      height:auto;
+      background: 	#90EE90;
+  }}
+
+  #score {{
+    width: 30px;
+    font-size: 120pt;
+  }}
+
+  #x {{
+    width: 30px;
+    font-size: 80pt;
+  }}
+
+  #team {{
+    font-size: 40pt;
+  }}
+
+</style>
+<body>
+
+  <h1>Rodada {round} - Jogo {game}</h1>
+
+  <table>
+    <tr>
+      <td><img src={home_src} alt=""></td>
+      <td></td>
+      <td id="x">X</td>
+      <td></td>
+      <td><img src={away_src} alt=""></td>
+    </tr>
+    <tr>
+      <td id="team">{home_team}</td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td id="team">{away_team}</td>
+    </tr>
+    <tr>
+      <td id="team">{home_twitter}</td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td id="team">{away_twitter}</td>
+    </tr>
+  </table>
+
+</body>
+"""
+
+SCORE_HTML = """<style>
+
+  body {{
+    color: #FFFFFF;
+    background: 	#90EE90;
+    font-family: Helvetica, Arial, sans-serif;
+    text-align: center;
+  }}
+
+  table {{
+    border-spacing: 20;
+    text-align: center;
+    width:70%;
+    margin-left:15%;
+    margin-right:15%;
+  }}
+
+  td, th {{
+    border: 1px solid transparent;
+    height: 30px;
+  }}
+
+  img {{
+      width: 300px;
+      height:auto;
+      background: 	#90EE90;
+  }}
+
+  #score {{
+    width: 30px;
+    font-size: 120pt;
+  }}
+
+  #x {{
+    width: 30px;
+    font-size: 80pt;
+  }}
+
+  #team {{
+    font-size: 40pt;
+  }}
+
+</style>
+<body>
+
+  <h1>Rodada {round} - Jogo {game}</h1>
+
+  <table>
+    <tr>
+      <td><img src={home_src} alt=""></td>
+      <td id="score">{home_score}</td>
+      <td id="x">X</td>
+      <td id="score">{away_score}</td>
+      <td><img src={away_src} alt=""></td>
+    </tr>
+    <tr>
+      <td id="team">{home_team}</td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td id="team">{away_team}</td>
+    </tr>
+    <tr>
+      <td id="team">{home_twitter}</td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td id="team">{away_twitter}</td>
+    </tr>
+  </table>
+
+</body>
+"""
